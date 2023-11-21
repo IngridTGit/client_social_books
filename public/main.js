c2ad71b0efe3ed0831278dc28f1b79506326ea80
Ingrid <ingrid.torres.ferreira@gmail.com>
2023-11-21 19:53:57 -0300

    Aula 01

--- public/main.js
+++ public/main.js
@@ -1,27 +1,24 @@
 const gitHubId = document.querySelector('input[type=text]')
 const btnBuscar = document.querySelector('input[type=button]')
-gitHubId.oninput = function(){
+btnBuscar.onclick = function(){
     //Fazendo a conexão com a API
     fetch('https://api.github.com/users/' + gitHubId.value)
-    .then(Response => {
-     if(Response.ok){
-        return Response.json()
-     }
-     else{
-        throw new Error('Erro na requisição')
-     }
+    .then(response => {
+        if(response.ok){
+            return response.json()
+        }
+        else {
+            throw new Error('Erro na requisição.')
+        }
     })
     .then(data => {
-      document.querySelector('#name').textContent = data.name
-      document.querySelector('#Bio').textContent = data.bio
-      document.querySelector('#Follwers').textContent = data.follwers
-      document.querySelector('#Location').textContent = data.location
-      document.querySelector('#Avatar').src = data.avatar_url
-
+        document.querySelector('#name').textContent = data.name
+        document.querySelector('#bio').textContent = data.bio
+        document.querySelector('#followers').textContent = data.followers
+        document.querySelector('#location').textContent = data.location
+        document.querySelector('#avatar').src = data.avatar_url
+        //E outros atribuitos que vc desejar
     })
-    .catch(Error => {
-        console.log('Erro' + Error)
-    })
-        
-
-}
+    .catch(error => {
+        console.log('Erro: '+ error)
+    }) }
